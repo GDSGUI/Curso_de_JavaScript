e2dcff465cf99cd2880889360fb5dae4de48197f
GDSGUI <86115314+GDSGUI@users.noreply.github.com>
2022-02-03 15:14:07 -0300

    Delete Aula011 directory

--- Aula011/Ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 60.5
-console.log (` A Velocidade do seu carro é ${vel} km/h`);
-if (vel > 60) {
-    console.log('Você Ultrapassou a Velocidade Permitida. MULTADO!')
-}
-console.log ('Dirija Sempre usando Cinto de Segurança');
--- Aula011/Ex009.js
+++
@@ -1,7 +0,0 @@
-var pais = 'Brasil';
-console.log (`Vivendo no(a) ${pais}`);
-if (pais != 'Brasil'){
-    console.log('Você é Estrangeiro');
-}   else {
-    console.log (`Você é Brasileiro`);
-}
